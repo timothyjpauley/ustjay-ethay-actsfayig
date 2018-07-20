00ab3c72709fa2da8f6630b3e86b535ffd2fcebd
Ruohan <tangtangruohan@hotmail.com>
2018-07-19 21:41:51 -0700

    pig latin fact

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, redirect, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 
 def get_fact():
@@ -16,13 +17,21 @@ def get_fact():
 
     return facts[0].getText()
 
+def piglatin():
 
-@app.route('/')
+    url = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
+    fact = get_fact()
+    payload = {"input_text": fact}
+    new_url = requests.post(url, allow_redirects=False, data=payload).headers.get('Location')
+    return new_url
+
+@app.route('/', methods=['GET'])
 def home():
-    return "FILL ME!"
+    url = piglatin()
+    print(url)
+    return render_template('piglatin.jinja2', url = url)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
